386ed192c138661550070501d62056003916e4e0
TheOkayDev <72171476+Sable-20@users.noreply.github.com>
2021-11-16 13:24:40 -0500

    added cobol lel

---
+++ code_samples/cobol.js
@@ -0,0 +1,12 @@
+samplelist.cobol = [
+  {
+    "name": "Hello, World",
+    "code": `IDENTIFICATION DIVISION.
+PROGRAM-ID. HELLO.
+
+PROCEDURE DIVISION.
+  DISPLAY 'Hello, World!'.
+STOP RUN
+`
+ }
+]
